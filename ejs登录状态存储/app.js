534833e91e27d00ab8a059d1dd8ab7c541f45818
NNNNzs <709934831@qq.com>
2019-02-19 21:03:05 +0800

    2.19

--- ejs登录状态存储/app.js
+++ ejs登录状态存储/app.js
@@ -13,39 +13,65 @@ app.set('view engine', 'html'); // 设置解析模板文件类型：这里为htm
 app.engine('html', require('ejs').__express); // 使用ejs引擎解析html文件中ejs语法
 
 app.use(bodyparser.json()); // 使用bodyparder中间件，
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
 // 使用 session 中间件
 app.use(session({
-    secret :  'secret', // 对session id 相关的cookie 进行签名
-    resave : true,
+    secret: 'secret', // 对session id 相关的cookie 进行签名
+    resave: true,
+    name:'NNNNzs_session',
     saveUninitialized: false, // 是否保存未初始化的会话
-    cookie : {
-        maxAge : 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
+    cookie: {
+        maxAge: 1000 * 60 * 3, // 设置 session 的有效时间，单位毫秒
     },
 }));
 
 // 获取登录页面
-app.get('/login', function(req, res){
+app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/login.html')
 });
+let users = [{
+    userName: 'admin',
+    pwd: '123'
+}, {
+    userName: 'admin1',
+    pwd: '123'
+}, {
+    userName: 'admin2',
+    pwd: '123'
+}]
 
 // 用户登录
-app.post('/login', function(req, res){
-    if(req.body.username == 'admin' && req.body.pwd == 'admin123'){
-        req.session.userName = req.body.username; // 登录成功，设置 session
-        res.redirect('/');
+app.post('/login', function (req, res) {
+    function checkAccount() {
+        return users.some(e=>{
+            return e.userName==req.body.username&&e.pwd==req.body.pwd
+        })
+        // return false;
     }
-    else{
-        res.json({ret_code : 1, ret_msg : '账号或密码错误'});// 若登录失败，重定向到登录页面
+    console.log(checkAccount())
+    if (checkAccount()) {
+        // if(req.body.username == 'admin' && req.body.pwd == 'admin123'){
+        req.session.userName = req.body.username; // 登录成功，设置 session
+        console.log(req.session)
+        res.redirect('/');
+    } else {
+        res.json({
+            ret_code: 1,
+            ret_msg: '账号或密码错误'
+        }); // 若登录失败，重定向到登录页面
     }
 });
 
 // 获取主页
 app.get('/', function (req, res) {
-    if(req.session.userName){  //判断session 状态，如果有效，则返回主页，否则转到登录页面
-        res.render('home',{username : req.session.userName});
-    }else{
+    if (req.session.userName) { //判断session 状态，如果有效，则返回主页，否则转到登录页面
+        res.render('home', {
+            username: req.session.userName
+        });
+    } else {
         res.redirect('login');
     }
 })
@@ -56,6 +82,6 @@ app.get('/logout', function (req, res) {
     res.redirect('login');
 });
 
-app.listen(8000,function () {
+app.listen(8000, function () {
     console.log('http://127.0.0.1:8000')
 })
