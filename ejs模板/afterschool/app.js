e35e24017aaf34aaf0e481ae97a292a7f2ab1d1d
NNNNzs <709934831@qq.com>
2019-03-02 18:57:38 +0800

    3.2代码

--- ejs模板/afterschool/app.js
+++ ejs模板/afterschool/app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login')
 const apiRouter = require('./routes/api')
+const productRouter = require('./routes/product')
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/product',productRouter)
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/api', apiRouter);
