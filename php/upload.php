97f83cb98302da84a018ec367b4ac992885e63f7
NNNNzs <709934831@qq.com>
2019-08-10 18:03:29 +0800

    上传接口

--- php/upload.php
+++ php/upload.php
@@ -13,14 +13,29 @@ $baseUrl = $requestScheme . '://' . $host;
 if ($_FILES["file"]["error"] > 0) {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
 } else {
-    $fileUrl =  $baseUrl . "/upload/" . $_FILES["file"]["name"];
+    $allowedExts = array("gif", "jpeg", "jpg", "png");
+    $temp = explode(".", $_FILES["file"]["name"]);
+    $extension = end($temp);        // 获取文件后缀名
+    $fileType = $_FILES["file"]["type"];
+
+    //默认
+    $fileUrl =  $baseUrl . "/upload/" . $_FILES["file"]["name"];//外网绝对路径
+    $absUrl = "upload/" . $_FILES["file"]["name"];//相对当前的路径
+    
+    //如果是图片，文件名是md5
+    if(substr($fileType,0,5)==='image'){
+        $fileFullName =  md5_file($_FILES["file"]["tmp_name"]).'.'.$extension;
+        $fileUrl = $baseUrl . "/upload/img/" . $fileFullName;
+        $absUrl = "upload/img/" .$fileFullName;
+    }
+    //非图片的文件名不能重复
     if (file_exists("upload/" . $_FILES["file"]["name"])) {
         $result = array('code' => 1001, 'content'=>$fileUrl,'msg' => '文件名已重复');
         echo json_encode($result);
     } else {
         move_uploaded_file(
             $_FILES["file"]["tmp_name"],
-            "upload/" . $_FILES["file"]["name"]
+            $absUrl
         );
         $result = array('code' => 1000, 'content' => $fileUrl, 'msg' => '上传成功');
         echo json_encode($result);
